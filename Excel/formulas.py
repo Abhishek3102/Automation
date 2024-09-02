1fb23dbcd7402f52aa35e74c37d39fcff0d3c79c
Abhishe16 <ankushpan694@gmail.com>
2024-09-02 11:07:36 +0530

    updated formulas file

--- Excel/formulas.py
+++ Excel/formulas.py
@@ -1,17 +1,17 @@
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
-wb = load_workbook('barchart.xlsx')
-sheet = wb['Report']
+workbook = load_workbook('barchart.xlsx')
+sheet = workbook['Report']
 
-min_column = wb.active.min_column
-max_column = wb.active.max_column
-min_row = wb.active.min_row
-max_row = wb.active.max_row
+min_column = workbook.active.min_column
+max_column = workbook.active.max_column
+min_row = workbook.active.min_row
+max_row = workbook.active.max_row
 
 for i in range(min_column+1, max_column+1):  
     letter = get_column_letter(i)
     sheet[f'{letter}{max_row + 1}'] = f'=SUM({letter}{min_row + 1}:{letter}{max_row})'
     sheet[f'{letter}{max_row + 1}'].style = 'Currency'
 
-wb.save('report.xlsx')
+workbook.save('report.xlsx')
