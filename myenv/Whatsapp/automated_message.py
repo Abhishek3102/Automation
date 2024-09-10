e3008dfaaf5f744f7d2c38f896d81672f3b6e627
Abhishe16 <ankushpan694@gmail.com>
2024-09-10 15:58:01 +0530

    sent whatsapp message to OG CHAMAR NO.2

--- myenv/Whatsapp/automated_message.py
+++ myenv/Whatsapp/automated_message.py
@@ -3,4 +3,4 @@ import pywhatkit
 # Send message to a contact
 phone_number = input("Enter phone number: ")
 
-pywhatkit.sendwhatmsg(phone_number, "Ludo Chamar", 11, 13)
+pywhatkit.sendwhatmsg(phone_number, "Ludo Chamar", 21, 55)
